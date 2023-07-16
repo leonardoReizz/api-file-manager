4ca1a073a59065a377a8671a855adf69e76fa1ab
Leonardo Reis <amarantee.reis@gmail.com>
2023-07-16 17:29:50 -0300

    feat: change function return

--- src/components/file/use-cases/save-file/save-file-use-case.ts
+++ src/components/file/use-cases/save-file/save-file-use-case.ts
@@ -1,11 +1,10 @@
 import { FileRepository } from "../../repositories/file-repository";
 import { ISaveFileRequestDTO } from "./save-file-request-dto";
-
 export class SaveFileUseCase {
   constructor(private fileRepository: FileRepository) {}
   async execute(data: ISaveFileRequestDTO) {
-    const id = await this.fileRepository.saveFile(data);
+    const file = await this.fileRepository.saveFile(data);
 
-    return id;
+    return { file, folderId: data.folderId };
   }
 }
