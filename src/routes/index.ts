79e02a169ba69930abab196c229be07370edb297
Leonardo Reis <amarantee.reis@gmail.com>
2023-07-16 17:25:39 -0300

    feat: swagger routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,17 @@ import { fileRoutes } from "./file.routes";
 import { userRoutes } from "./user.routes";
 import { folderRoutes } from "./folder.routes";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../swagger-output.json";
+
 const routes = Router();
 
 routes.use("/authenticate", authenticationRoutes);
 routes.use("/user", userRoutes);
 routes.use("/file", fileRoutes);
 routes.use("/folder", folderRoutes);
+
+routes.use("/docs", swaggerUi.serve);
+routes.get("/docs", swaggerUi.setup(swaggerDocument));
 
 export { routes };
